6bf26845bec35e17f6fca18e751179fdce785218
Emile Achadde <achadde@iabloko.fecamp>
2020-02-22 17:48:57 +0100

    gradle no_variable

--- src/main/kotlin/io/ipfs/kotlin/IpfsCommands.kt
+++ src/main/kotlin/io/ipfs/kotlin/IpfsCommands.kt
@@ -3,10 +3,48 @@ package io.ipfs.kotlin
 import java.util.Stack
 
 /**
- * Execution : gradlew run --args="-ipfs get QmTzX91dhqHRunjCtrt4LdTErREUA5Gg1wFMiJz1bEiQxp"
- *
+ * Execution : gradlew run --args="-ipfs get QmTzX91dhqHRunjCtrt4LdTErREUA5Gg1wFMiJz1bEiQxp" val multihash = LocalIPFS().add.string("test-string").Hash
+ * val content = LocalIPFS().get.cat(multihash)
+ * val commit = LocalIPFS().info.version()!!.Commit
  * Author : François Colonna 22 février 2020 at 15:32:44+01:00
  */
+
+fun ipfsExecuteOfWordList(wor_l: List<String>) {
+    val (here, caller) = hereAndCaller()
+    entering(here, caller)
+    
+    // Ex.: -ipfs add truc much
+    var done = false
+    if(isTrace(here)) println ("$here: input wor_l '$wor_l'")
+    var wor_s = wordStackOfWordList(wor_l)
+    
+    while (!done) {
+	try {
+	    val wor = wor_s.pop()
+	    val wor_3 = wor.substring(0,3)
+	    println("$here: wor '$wor'")
+	    
+	    when (wor_3) {
+		"add" -> {
+		    val hash = ipfsHashOfAddWordStack(wor_s)
+		    println ("Hash: $hash")
+		}
+		"get" -> {
+		        wor_s.clear()
+			val immCon = ipfsImmutableContentOfGetWordList(wor_l)
+			println ("Content:")
+			println (immCon.toString())
+    		}
+		else -> {
+		    fatalErrorPrint ("command were 'add', 'get'","'"+wor+"'", "Check input", here)
+		} // else
+	    } // when
+	} // try
+	catch (e: java.util.EmptyStackException) {done = true} // catch
+	
+    } // while
+    exiting(here)
+}
 
 fun ipfsHashOfAddWordStack (wor_s: Stack<String>): IpfsHash {
     val (here, caller) = hereAndCaller()
@@ -55,40 +93,4 @@ fun ipfsImmutableContentOfGetWordList (wor_l: List<String>): IpfsImmutableConten
     return result
 }
 
-fun ipfsExecuteOfWordList(wor_l: List<String>) {
-    val (here, caller) = hereAndCaller()
-    entering(here, caller)
-    
-    // Ex.: -ipfs add truc much
-    var done = false
-    if(isTrace(here)) println ("$here: input wor_l '$wor_l'")
-    var wor_s = wordStackOfWordList(wor_l)
-    
-    while (!done) {
-	try {
-	    val wor = wor_s.pop()
-	    val wor_3 = wor.substring(0,3)
-	    println("$here: wor '$wor'")
-	    
-	    when (wor_3) {
-		"add" -> {
-		    val hash = ipfsHashOfAddWordStack(wor_s)
-		    println ("Hash: $hash")
-		}
-		"get" -> {
-		        wor_s.clear()
-			val immCon = ipfsImmutableContentOfGetWordList(wor_l)
-			println ("Content:")
-			println (immCon.toString())
-    		}
-		else -> {
-		    fatalErrorPrint ("command were 'add', 'get'","'"+wor+"'", "Check input", here)
-		} // else
-	    } // when
-	} // try
-	catch (e: java.util.EmptyStackException) {done = true} // catch
-	
-    } // while
-    exiting(here)
-}
 
